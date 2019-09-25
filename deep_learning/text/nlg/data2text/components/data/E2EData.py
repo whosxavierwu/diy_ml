1218527d7f19c116c7060a8ed67ed685e1cfb68d
whosxavierwu <whosxavierwu@gmail.com>
2019-09-25 09:38:21 +0800

    update

--- deep_learning/text/nlg/data2text/components/data/E2EData.py
+++ deep_learning/text/nlg/data2text/components/data/E2EData.py
@@ -11,16 +11,29 @@ class E2EData(BaseData):
         super().__init__()
 
     def setup(self, configs: dict):
+        """
+        prepare everything we need after
+        :param configs:
+        :return:
+        """
         train_filename = configs['train_filename']
-        df = pd.read_csv(train_filename, sep=',')
+        df = pd.read_csv(train_filename, sep=',')  # mr, ref
         for _, r in df.iterrows():
-            # todo working here
-            pass
+            record = BaseRecord()
+            record.ref = r['ref']
+            for kv in r['mr'].split(','):
+                parts = kv.replace(']', '').split('[')
+                feat_name = parts[0].strip()
+                feat_value = parts[1].strip()
+                record.mr[feat_name] = feat_value
+            self.records.append(record)
+        print('Data loaded: ', len(self.records))
 
     def save(self):
         pass
 
 
 if __name__ == '__main__':
-    pass
+    D = E2EData()
+    D.setup({'train_filename': '../../dataset/e2e-dataset/trainset.csv'})
 
